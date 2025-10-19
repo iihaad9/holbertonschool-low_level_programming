7b9775759a953f22be7e261e07bc79939a040a89
iihaad9 <fahadfawaz5@gmail.com>
2025-10-19 06:13:26 +0000

    haad

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
-#include "main.hه"
+#include "main.h"
 /**
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  *
- * Return: void
+ * Return: nothing
  */
 void print_alphabet_x10(void)
 {
