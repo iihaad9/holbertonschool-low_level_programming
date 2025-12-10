ff28f188fbabf9e7a2d06090f8c12415e72b0968
iihaad9 <fahadfawaz5@gmail.com>
2025-12-10 14:23:11 +0000

    haad

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -22,15 +22,30 @@ printf("0\n");
 return (0);
 }
 while (cents >= 25)
-cents -= 25, coins++;
+{
+cents -= 25;
+coins++;
+}
 while (cents >= 10)
-cents -= 10, coins++;
+{
+cents -= 10;
+coins++;
+}
 while (cents >= 5)
-cents -= 5, coins++
+{
+cents -= 5;
+coins++;
+}
 while (cents >= 2)
-cents -= 2, coins++;
+{
+cents -= 2;
+coins++;
+}
 while (cents >= 1)
-cents -= 1, coins++;
+{
+cents -= 1;
+coins++;
+}
 printf("%d\n", coins);
 return (0);
 }
