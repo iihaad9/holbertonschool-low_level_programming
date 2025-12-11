e42ce9659eb7952d4578acdb4312a90434345561
iihaad9 <fahadfawaz5@gmail.com>
2025-12-11 14:53:30 +0000

    haad

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 #endif
