1bd07f7135c8d58f4fe423f0cecd80e0862c5abb
iihaad9 <fahadfawaz5@gmail.com>
2025-12-22 15:04:57 +0000

    haad

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,11 +2,13 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-#include <stddef.h>
+
+int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
 
