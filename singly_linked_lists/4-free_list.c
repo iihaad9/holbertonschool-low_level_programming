3a385aad44c74ab50c5ba28565cce6ac2ba3c011
iihaad9 <fahadfawaz5@gmail.com>
2025-12-22 16:18:57 +0000

    Add free_list function

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node
+ */
+void free_list(list_t *head)
+{
+list_t *tmp;
+while (head != NULL)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
+}
