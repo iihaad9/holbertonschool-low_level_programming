a0480a2cf4ebe3b38d5720a859f09600462e3926
iihaad9 <fahadfawaz5@gmail.com>
2025-12-22 15:28:27 +0000

    add

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,16 @@
+global main
+
+section .data
+msg db "Hello, World", 10
+len equ $ - msg
+
+section .text
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, len
+	syscall
+
+	mov rax, 0
+	ret
