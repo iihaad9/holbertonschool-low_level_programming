ab81fe05b990fa4a028b6c91e6774fd33973b1b2
iihaad9 <fahadfawaz5@gmail.com>
2025-12-23 14:26:12 +0000

    haad

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -22,4 +22,12 @@ struct dlistint_s *next;
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h);
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new node, or NULL if it fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 #endif
