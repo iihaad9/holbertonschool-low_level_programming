88d07ef7af72e1f0f5b507a7e5c87e5eb3959d1d
iihaad9 <fahadfawaz5@gmail.com>
2025-12-23 16:33:56 +0000

    l

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+void close_fd(int fd);
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
 /**
  * main - copies the content of a file to another file
  * @ac: argument count
@@ -12,8 +14,6 @@
 int main(int ac, char **av)
 {
 int fd_from, fd_to;
-ssize_t r, w;
-char buf[1024];
 if (ac != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -32,33 +32,46 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 close(fd_from);
 exit(99);
 }
+copy_file(fd_from, fd_to, av[1], av[2]);
+close_fd(fd_from);
+close_fd(fd_to);
+return (0);
+}
+/**
+ * copy_file - copies content from one file descriptor to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+ssize_t r, w;
+char buf[1024];
 while ((r = read(fd_from, buf, 1024)) > 0)
 {
 w = write(fd_to, buf, r);
 if (w == -1 || w != r)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-close(fd_from);
-close(fd_to);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 exit(99);
 }
 }
 if (r == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-close(fd_from);
-close(fd_to);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 exit(98);
 }
-if (close(fd_from) == -1)
+}
+/**
+ * close_fd - closes a file descriptor
+ * @fd: file descriptor
+ */
+void close_fd(int fd)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
-return (0);
 }
