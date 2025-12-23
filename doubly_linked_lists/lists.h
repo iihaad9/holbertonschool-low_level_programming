ad0239d04226a2507affcd2a4be9c98a91fcf4a2
iihaad9 <fahadfawaz5@gmail.com>
2025-12-23 14:30:39 +0000

    k

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,10 +13,19 @@
  */
 typedef struct dlistint_s
 {
-int n;
-struct dlistint_s *prev;
-struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
  * @h: pointer to the head of the list
@@ -22,6 +33,7 @@ struct dlistint_s *next;
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h);
+
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  * @head: pointer to the head of the list
@@ -30,4 +42,5 @@ size_t dlistint_len(const dlistint_t *h);
  * Return: address of the new node, or NULL if it fails
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
 #endif
