58b930f8077e067aa4f6511d3382cdab0c41dbe8
iihaad9 <fahadfawaz5@gmail.com>
2025-12-23 13:55:37 +0000

    haad

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+/**
+ * init_dog - initialize a dog structure
+ * @d: pointer to struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
