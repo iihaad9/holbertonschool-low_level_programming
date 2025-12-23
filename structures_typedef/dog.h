03f13c10f558bb513b4f743dd0e8cdb4f77cda5d
iihaad9 <fahadfawaz5@gmail.com>
2025-12-23 13:52:02 +0000

    haad

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog - information about a dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
+
