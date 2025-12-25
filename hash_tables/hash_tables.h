d13924db6e851d710d3241b1cca21a4d999c45dc
iihaad9 <fahadfawaz5@gmail.com>
2025-12-25 15:11:03 +0000

    l

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -28,5 +28,4 @@ hash_node_t **array;
 /* Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-
 #endif /* HASH_TABLES_H */
