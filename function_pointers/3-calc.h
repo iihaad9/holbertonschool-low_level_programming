af5f029b80f525e3492d3fe4c101c2f71cafc2ec
iihaad9 <fahadfawaz5@gmail.com>
2025-12-25 17:12:46 +0000

    l

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,9 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
-#include <stdlib.h>
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,10 @@
 #include "3-calc.h"
+/**
+ * get_op_func - selects the correct operation function
+ * @s: operator passed as argument
+ *
+ * Return: pointer to the matching function or NULL
+ */
 int op_add(int a, int b)
 {
 return (a + b);
