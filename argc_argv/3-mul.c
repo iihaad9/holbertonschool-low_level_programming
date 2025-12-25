bf909d514b061551a71d571f3e7b1ef84414d798
iihaad9 <fahadfawaz5@gmail.com>
2025-12-25 16:48:59 +0000

    Fix Betty style for task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -5,19 +5,19 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 if success, 1 if error
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-int n1, n2, result;
+int num1, num2, result;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-result=n1*n2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
