7338c0decfa7a54994dd730c164cbb82052fa080
iihaad9 <fahadfawaz5@gmail.com>
2025-12-25 16:20:14 +0000

    l

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -40,6 +40,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 /* Task 3 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
 
